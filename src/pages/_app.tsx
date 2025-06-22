b11f6f919a6f28f7f873cb160c05a8b7ce6347ec
ShacharZ <shachar.zuk@gmail.com>
2025-06-22 20:41:01 +0300

    feat: google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { AppProps } from "next/app";
 import "../lib/styles/app.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <GoogleAnalytics gaId="G-KMMXPJXFVP" />
+    </>
+  );
 }
 
 export default MyApp;
